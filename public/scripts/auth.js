30ffdad396293859cc8550f9bfad6b70c07a46d7
Kevin McGillicuddy <Kevin.McGillicuddy@sight-sound.com>
2020-04-07 21:13:35 -0400

    firebase hosting

---
+++ public/scripts/auth.js
@@ -0,0 +1,57 @@
+const loginForm = document.querySelector('#login-form')
+const logoutButton = document.querySelector('#logout')
+const GoogleloginButton = document.querySelector('#login-form-google')
+const signupForm = document.querySelector('#signup-form')
+//listen for uath state changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+        setupUI(user)
+    }
+    else {
+        setupUI(user)
+    }
+})
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+        auth.signInWithEmailAndPassword(email, password).then((response) => {
+            const modal = document.querySelector('#modal-login');
+            M.Modal.getInstance(modal).close();
+            loginForm.reset();
+        })
+        .catch((error)=>{
+            alert(errorMessage)
+            console.error(error)
+        })
+})
+
+//logout users
+logoutButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut()
+})
+
+//login user google
+GoogleloginButton.addEventListener('click', () => {
+    auth.signInWithRedirect(provider)
+        .catch(function (error) {
+            console.log(error)
+        });
+})
+
+//signup user
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+    auth.createUserWithEmailAndPassword(email, password).then((response) => {
+        return db.collection('users').doc(response.user.uid)
+    }).then(() => {
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    })
+})
---
+++ public/scripts/dom.js
@@ -0,0 +1,61 @@
+var storageRef = storage.ref()
+var listRef = storageRef.child('mp3/2020')
+const list = document.querySelector('.list')
+const accountDetails = document.querySelector('.account-details')
+const loggedOutLinks = document.querySelectorAll('.logged-out')
+const loggedInLinks = document.querySelectorAll('.logged-in')
+
+// setup materialize components
+
+document.addEventListener('DOMContentLoaded', function () {
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+  var items = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(items);
+});
+
+
+//setup nav links based on user log in / log out
+const setupUI = (user) => {
+  if (user) {
+
+    //setup account info
+    const html = `<div><img src="${user.photoURL}" style="border-radius:50%; height:100px" />
+      <p><div>Logged in as: <b>${user.email}</b></div>`
+    accountDetails.innerHTML = html
+    //toggle UI elements
+    loggedInLinks.forEach(item => item.style.display = "block")
+    loggedOutLinks.forEach(item => item.style.display = "none")
+
+      //create list
+    listRef.listAll()
+      .then((response) => {
+        let html = ''
+        response.items.forEach(itemRef => {
+          var dlRef = storageRef.child(itemRef.fullPath)
+          dlRef.getDownloadURL()
+            .then(function (url) {
+              console.log(url)
+              const li =
+                `<li>
+              <div class="collapsible-header grey lighten-4">${itemRef.name}</div>
+              <div class="collapsible-body white"><img src="image/download-button.png" width="25" height="25" download="${url}"/>
+              </div>
+             </li>`
+              html += li
+              list.innerHTML = html;
+            })
+            .catch(function (error) { console.log(error) })
+        });
+      }).catch(error => console.log(error));
+  }
+  else {
+    //hide account info
+    accountDetails.innerHTML = ''
+    //toggle UI elements
+    loggedInLinks.forEach(item => item.style.display = "none")
+    loggedOutLinks.forEach(item => item.style.display = "block")
+  }
+}
+
+
