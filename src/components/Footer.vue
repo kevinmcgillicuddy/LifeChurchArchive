9b018dbeb02403d2ba76f17e7fbe7e10adc9c44a
Kevin McGillicuddy <Kevin.McGillicuddy@sight-sound.com>
2020-08-04 12:48:36 -0400

    added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,54 @@
+<template>
+     <footer class="page-footer-main">
+          <div class="container footermain">
+            <div class="row footermain">
+              <div class="col l6 s12">
+                <h5 class="white-text">Life Church Lancaster</h5>
+                <p class="grey-text text-lighten-4"><img src="https://images.squarespace-cdn.com/content/v1/5b8959b796e76f42f164a8b4/1544566935575-UNOD1ACTMVIW5VGHNGD6/ke17ZwdGBToddI8pDm48kGwrK3btlI3EWQwJ6Y0kNmmoCXeSvxnTEQmG4uwOsdIceAoHiyRoc52GMN5_2H8Wp8yrVxZlvyGiJVRFXUmhbI-ctUEcr85o6auTtd2MWfNSpJS6KsTJ3kPRsKlQIsIu4A/faded+icon.png?format=100w" /></p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  node<li><a class="grey-text text-lighten-3" href="https://lifechurchlancaster.org">Life Church</a></li>
+                  <li><a class="grey-text text-lighten-3" href="https://lifechurchlancaster.org/sermons">Sermons</a></li>
+                  <li><a class="grey-text text-lighten-3" href="https://lifechurchlancaster.org/app">The App</a></li>
+                  <li><a class="grey-text text-lighten-3" href="https://lifechurchlancaster.org/contact">Contact</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer">
+            <div class="container row footer">
+                <div class="col s6"><p>Sundays 9:00am and 10:45am</p><p>700 Brentwood Drive. Lititz, PA 17543</p></div>
+                <div class="col s6"><p>(717) 723-8194</p><p>admin@lifechurchlancaster.org </p></div>
+     
+            </div>
+          </div>
+        </footer>
+</template>
+<script>
+export default {
+    Name:'Footer',
+    data(){
+        return{}
+    }
+}
+</script>
+<style>
+body {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+  main {
+    flex: 1 0 auto;
+  }
+  .footer{
+      background-color: #38525c;
+  }
+  .page-footer-main{
+      background-color: #5d737e;
+      padding-top: 20px;
+      color: #fff;
+  }
+</style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -15,6 +15,10 @@
                 <button class="btn blue darken-3">Login</button>
             </div>
         </form>
+    <!-- <div clas="divider"></div>
+    
+        <a class="waves-effect waves-light btn" @click="gLogin"><i class="material-icons left">account_circle</i>Sign in with Google</a> -->
+        
     </div>
 </template>
 <script>
@@ -38,7 +42,17 @@ export default {
             }).catch(err=> {this.feedback =  err.message})
           }
           else{this.feedback = "You must enter a email and password"}
-      }
+      },
+    //  gLogin(){
+    //      var provider = new firebaseApp.auth.GoogleAuthProvider();
+    //      firebaseApp.auth().signInWithPopup(provider).then((result) => {
+    //           var token = result.credential.accessToken;
+    //           var user = result.user;
+    //         }).catch(err=> {
+    //             var errorCode = error.code;
+    //             var errorMessage = error.message;
+    //             var email = error.email;
+    //             var credential = error.credential;}); }
   }
   }
 </script>
