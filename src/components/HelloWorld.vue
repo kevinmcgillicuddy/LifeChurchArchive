0c51cdd25f63b51194ec5331b7896dfce96c9cc4
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2020-08-17 22:37:05 -0400

    commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,9 @@
       <div class="container logged-in" style="margin-top: 40px;">
       <ul class="collapsible z-depth-0 list" style="border: none;">
         <li v-for="(file,index) in files" :key="index">
-          <div class="collapsible-header"><a :href="file.url.i"><i class="material-icons">filter_drama</i></a>{{ file.name }}</div>
+          <div class="collapsible-header"><a :href="file.url.i"><i class="material-icons">filter_drama</i></a>{{ file.name }}
+          <p>{{ file.path }}</p>
+          <button v-on:click="send(file.gsurl,file.name,$event)">Transcribe</button></div>
           <div class="collapsible-body"><span>{{ file.name }}</span></div>
         </li>
       </ul>
@@ -13,6 +15,8 @@
 </template>
 <script>
 import { storage } from '@/firebase/init.js'
+import { functions } from '@/firebase/init.js'
+import { db } from '@/firebase/init.js'
 import M from 'materialize-css'
 export default {
   name: 'HelloWorld',
@@ -21,27 +25,54 @@ export default {
       files: []
     }
   },
+  methods:{
+
+  },
    mounted(){  
       var CollapseElems = document.querySelectorAll('.collapsible');
       M.Collapsible.init(CollapseElems)
     
     },
   created(){
+      let guid = () => {
+          let s4 = () => {return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)}
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+      }
     var folder = 'mp3'
     var storageRef = storage.ref()
     var listRef = storageRef.child(folder)
-     listRef.listAll()
+    listRef.listAll()
             .then(response => {
                 let files = []  
                 response.items.forEach(item => {
-                     const url = item.getDownloadURL().then(url => {return url} )
-                     const gsurl = `gs://lcarchivewebsite.appspot.com/${folder}/${item.name}`
-                     files.push({...item, name:item.name, url, gsurl})
+                    const id = {uid: guid()}
+                    const url = item.getDownloadURL().then(url => {return url} )
+                    const gsurl = `gs://lcarchivewebsite.appspot.com/${folder}/${item.name}`
+                    files.push({...item, name:item.name, url, gsurl, id:id.uid})
                     });
                     this.files = files;
                   })
             .catch(error => console.log(error));
-  }
+  
+  
+  db.collection('sermons').get()
+  .then(snapshot => {
+    snapshot.forEach(book => {
+      let b = book.data()
+      b.id = book.id
+      books.push(b)
+    });
+  })  
+
+  
+  },
+    methods:{
+      send(file,name,event){
+      event.target.disabled = true
+      const transcribe = functions.httpsCallable('transcribe')
+      transcribe({ file: file, name: name }).then(()=>{alert("File submitted") })
+      }
+    }
 }
 </script>
 
