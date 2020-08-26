da786e958fa8016b9e0fdcd93a62885f6ee413f8
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2020-08-25 23:50:54 -0400

    formatting and uuid working

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,80 +1,159 @@
 <template>
-<div>
-<nav class="nav-extended header">
-    <div class="nav-wrapper">
-      <img src="https://images.squarespace-cdn.com/content/v1/5b8959b796e76f42f164a8b4/1544566935575-UNOD1ACTMVIW5VGHNGD6/ke17ZwdGBToddI8pDm48kGwrK3btlI3EWQwJ6Y0kNmmoCXeSvxnTEQmG4uwOsdIceAoHiyRoc52GMN5_2H8Wp8yrVxZlvyGiJVRFXUmhbI-ctUEcr85o6auTtd2MWfNSpJS6KsTJ3kPRsKlQIsIu4A/faded+icon.png?format=100w" />
-      <a href="https://lifechurchlancaster.org" class="brand-logo nb"><i class="material-icons"></i>Life Church Lancaster</a>
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li v-if="!user.loggedIn"><router-link :to="{name:'Login'}">Log In</router-link></li>
-        <li v-if="user.loggedIn"><a @click="logout">Log Out</a></li>
-        <li>  <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a></li>
-      </ul>
-    </div>
+  <div>
+    <nav class="nav-extended header">
+      <div class="nav-wrapper">
+        <img
+          src="../assets/logo.png"
+        />
+        <a href="https://lifechurchlancaster.org" class="brand-logo nb">
+          <i class="material-icons"></i>Life Church Lancaster
+        </a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+          <i class="material-icons">menu</i>
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li v-if="!user.loggedIn">
+            <router-link :to="{name:'Login'}">Log In</router-link>
+          </li>
+          <li v-if="user.loggedIn">
+            <a @click="logout">Log Out</a>
+          </li>
+          <li>
+            <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+          </li>
+        </ul>
+      </div>
       <ul class="sidenav page-header-nav" id="mobile-demo">
-        <li v-if="!user.loggedIn"><router-link :to="{name:'Login'}">Log In</router-link></li>
-        <li v-if="user.loggedIn"><a @click="logout">Log Out</a></li>
-        <li>  <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a></li>
-     </ul>
-  </nav>
+        <li v-if="!user.loggedIn">
+          <router-link :to="{name:'Login'}">Log In</router-link>
+        </li>
+        <li v-if="user.loggedIn">
+          <a @click="logout">Log Out</a>
+        </li>
+        <li>
+          <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+        </li>
+      </ul>
+    </nav>
 
-  <div id="modal1" class="modal">
-    <div class="modal-content">
-      <div>
-           <input type="file" ref="input1" style="display: none">                
-           <button>choose a photo</button>
-           
-         </div>
-</div>
-    <div class="modal-footer">
-      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
-    </div
-    >
-  </div>
+    <div id="modal1" class="modal">
+      <div class="modal-content">
+        <div>
+          <input type="file" @change="previewImage" />
+        </div>
+        <div>
+          <p>
+            Progress: {{uploadValue.toFixed()+"%"}}
+            <progress
+              id="progress"
+              :value="uploadValue"
+              max="100"
+            ></progress>
+          </p>
 
+          <!-- <div class="progress">
+            <div class="determinate" style={ width: uploadValue+}> <progress
+              id="progress"
+              max="100"
+            ></progress></div>
+          </div>-->
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button @click="onUpload" class="btn waves-effect waves-light">Upload</button>
+        <button class="btn waves-effect waves-light modal-close">Close</button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import M from 'materialize-css'
+import M from "materialize-css";
 import { mapGetters } from "vuex";
-import { auth } from '@/firebase/init.js'
+import { auth } from "@/firebase/init.js";
+import { storage } from "@/firebase/init.js";
 export default {
-    name: 'Header',
-    data(){
-        return{}
+  name: "Header",
+  data() {
+    return {
+      mp3Data: null,
+      // mp3: null,
+      uploadValue: 0,
+    };
+  },
+  methods: {
+    logout() {
+      auth.signOut().then(() => {
+        this.$router.push({ name: "/" });
+      });
     },
-    methods:{
-        logout(){
-            auth.signOut().then(()=>{
-               this.$router.push({name:'/'}) 
-            })
-        }
+    previewImage(event) {
+      this.uploadValue = 0;
+      // this.mp3 = null;
+      this.mp3Data = event.target.files[0];
     },
-    computed:{
-      ...mapGetters({    
-      user: "user"
-    })
-    },
-      mounted(){  
-      M.AutoInit()
+    onUpload() {
+      function guid() {
+        let s4 = () => {
+          return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+        };
+        return s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4();
       }
-}
+
+      var metadata = {
+        customMetadata: {
+          uuid: guid(),
+        },
+      };
+
+      this.mp3 = null;
+      const storageRef = storage
+        .ref(`mp3/${this.mp3Data.name}`)
+        .put(this.mp3Data, metadata);
+      storageRef.on(
+        `state_changed`,
+        (snapshot) => {
+          this.uploadValue =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        (error) => {
+          console.log(error.message);
+        }
+      );
+      // ()=>{this.uploadValue=100;
+      //   storageRef.snapshot.ref.getDownloadURL().then((url)=>{
+      //     this.picture =url;
+      //   });
+      // }
+      // );
+    },
+  },
+  computed: {
+    ...mapGetters({
+      user: "user",
+    }),
+  },
+  mounted() {
+    M.AutoInit();
+  },
+};
 </script>
 <style>
 body {
-    display: flex;
-    min-height: 100vh;
-    flex-direction: column;
-  }
-  main {
-    flex: 1 0 auto;
-  }
-  .header{
-      background-color: #38525c;
-  }
-  .page-header-nav{
-      background-color: #5d737e;
-      padding-top: 20px;
-      color: #fff;
-  }
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+main {
+  flex: 1 0 auto;
+}
+.header {
+  background-color: #38525c;
+}
+.page-header-nav {
+  background-color: #5d737e;
+  padding-top: 20px;
+  color: #fff;
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,97 +1,106 @@
 <template>
-<div>
-  <div class="container logged-in" style="margin-top: 40px;">
-       <ul class="collapsible z-depth-0 list" style="border: none;">
+  <div>
+    <div class="container logged-in" style="margin-top: 40px;">
+      <ul class="collapsible z-depth-0 list" style="border: none;">
         <li v-for="(file,index) in files" :key="index">
           <div class="row">
-            <div class="collapsible-header flow-text ">
-                <div class="col s10">
-                    <div>
-                        <a :href="file.url.i"><i class="material-icons">music_note</i></a>{{ file.name }}
-                    </div>
+            <div class="collapsible-header flow-text">
+              <div class="col s10">
+                <div>
+                  <a :href="file.url.i">
+                    <i class="material-icons">music_note</i>
+                  </a>
+                  {{ file.name }}
                 </div>
-                <div class="col s2 ">
-                    <div v-if="!file.text" class="info">
-                      <button class="btn-small white" v-on:click="send(file.gsurl,file.name,$event)"><span class="text">Transcribe</span></button>
-                    </div>
-                </div>        
+              </div>
+              <div class="col s2">
+                <div v-if="!file.text" class="info">
+                  <button class="btn-small white" v-on:click="send(file.gsurl,file.name,$event)">
+                    <span class="text">Transcribe</span>
+                  </button>
+                </div>
+              </div>
             </div>
-            </div>
-          <div class="collapsible-body flow-text"><span>{{ file.text }}</span></div>
+          </div>
+          <div class="collapsible-body flow-text">
+            <span>{{ file.text }}</span>
+          </div>
         </li>
       </ul>
-    </div> 
-    <div>
-
-</div>
-
-</div>
+    </div>
+    <div></div>
+  </div>
 </template>
 <script>
-import { storage } from '@/firebase/init.js'
-import { functions } from '@/firebase/init.js'
-import { firestore } from '@/firebase/init.js'
-import { mapGetters } from "vuex"
-import M from 'materialize-css'
+import { storage } from "@/firebase/init.js";
+import { functions } from "@/firebase/init.js";
+import { firestore } from "@/firebase/init.js";
+import { mapGetters } from "vuex";
+import M from "materialize-css";
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
       files: [],
-      string:'string',
-      upFiles:[]
-    }
+      string: "string",
+      upFiles: [],
+    };
   },
-   computed:{
-      ...mapGetters({
-      user: "user"
-    })
-    },
-   mounted(){  
-      M.AutoInit()
-      //  let guid = () => {
-      //     let s4 = () => {return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)}
-      // return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-      // }
-              // const id = {uid: guid()}
+  computed: {
+    ...mapGetters({
+      user: "user",
+    }),
+  },
+  mounted() {
+    M.AutoInit();
 
-    var folder = 'mp3'
-    var storageRef = storage.ref()
-    var listRef = storageRef.child(folder)   
+    var folder = "mp3";
+    var storageRef = storage.ref();
+    var listRef = storageRef.child(folder);
 
-  async function getFiles(){
-      let sermons = await listRef.listAll()
-      let files = []
-      for (const sermon of sermons.items){
-        const text = await getText(sermon.name)
-        const url = await sermon.getDownloadURL()
-        const gsurl = `gs://lcarchivewebsite.appspot.com/${folder}/${sermon.name}`
-        files.push({...sermon, name:sermon.name, url, gsurl, text})  
-    }
-    return files
-    }
-
-  async function getText(docID) {
-        var docRef = firestore.collection("sermons").doc(docID);
-        let doc = await docRef.get()
-        if (doc.exists){return await doc.data().text}}
-
-getFiles().then(res=>this.files = res)
-    
-    },
-    methods:{
-      send(file,name,event){
-      event.target.disabled = true
-      const transcribe = functions.httpsCallable('transcribe')
-      transcribe({ file: file, name: name }).then(()=>{alert("File submitted") })
+    async function getFiles() {
+      let sermons = await listRef.listAll();
+      let files = [];
+      for (const sermon of sermons.items) {
+        const md = await getMetadata(sermon)
+        // console.log(md.customMetadata.uuid)
+        const text = await getText(sermon.name);
+        const url = await sermon.getDownloadURL();
+        const gsurl = `gs://lcarchivewebsite.appspot.com/${folder}/${sermon.name}`;
+        files.push({ ...sermon, name: sermon.name, url, gsurl, text });
       }
-    }    
-}
+      return files;
+    }
+
+    async function getText(docID) {
+      var docRef = firestore.collection("sermons").doc(docID);
+      let doc = await docRef.get();
+      if (doc.exists) {
+        return await doc.data().text;
+      }
+    }
+    //ref should be sermon
+    async function getMetadata(ref) {
+      return ref.getMetadata()
+    }
+
+    getFiles().then((res) => (this.files = res));
+  },
+  methods: {
+    send(file, name, event) {
+      event.target.disabled = true;
+      const transcribe = functions.httpsCallable("transcribe");
+      transcribe({ file: file, name: name }).then(() => {
+        alert("File submitted");
+      });
+    },
+  },
+};
 </script>
 
 
 <style >
-.text{
+.text {
   color: black;
 }
 </style>
