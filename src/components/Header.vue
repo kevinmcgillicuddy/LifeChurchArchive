288908d830c23ea13e26f83f45c280cd4843c8cb
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2020-09-11 11:15:59 -0400

    added year support to upload and view

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <div>
     <nav class="nav-extended header">
       <div class="nav-wrapper">
-            <a href="https://lifechurchlancaster.org" class="brand-logo nb">
+        <a href="https://lifechurchlancaster.org" class="brand-logo nb">
           <i class="material-icons"></i>Life Church Lancaster Archive
         </a>
         <a href="#" data-target="mobile-demo" class="sidenav-trigger">
@@ -16,7 +16,10 @@
             <a class="waves-effect waves-light brand-color-dark btn" @click="logout">Log Out</a>
           </li>
           <li>
-            <a class="waves-effect waves-light btn modal-trigger white" href="#modal1"><i class="material-icons right text">add</i><span class="text">Upload</span></a>
+            <a class="waves-effect waves-light btn modal-trigger white" href="#modal1">
+              <i class="material-icons right text">add</i>
+              <span class="text">Upload</span>
+            </a>
           </li>
         </ul>
       </div>
@@ -40,6 +43,32 @@
         </div>
         <div>
           <p></p>
+          <form action="#">
+            <p>
+              <label>
+                <input name="group1" value="2020" type="radio" v-model="yearPicked"  />
+                <span>2020</span>
+              </label>
+            </p>
+            <p>
+              <label>
+                <input name="group1"  value="2019" type="radio"  v-model="yearPicked"/>
+                <span>2019</span>
+              </label>
+            </p>
+            <p>
+              <label>
+                <input class="with-gap" name="group1" value="2018" type="radio" v-model="yearPicked" />
+                <span>2018</span>
+              </label>
+            </p>
+            <p>
+              <label>
+                <input name="group1" value="2017" type="radio" v-model="yearPicked"/>
+                <span>2017</span>
+              </label>
+            </p>
+          </form>
           <div v-if="mp3Data" class="progress">
             <div class="determinate" v-bind:style="{ width: uploadValue + '%'}"></div>
           </div>
@@ -64,7 +93,8 @@ export default {
     return {
       mp3Data: null,
       uploadValue: 0,
-      feedback:''
+      feedback: "",
+      yearPicked: null
     };
   },
   methods: {
@@ -75,14 +105,12 @@ export default {
     },
     previewFile(event) {
       this.uploadValue = 0;
-      let type = "audio"
-      if(! event.target.files[0].type.includes(type))
-      {
-        this.feedback = "Please upload an audio file"
-        }
-        else{
-           this.mp3Data = event.target.files[0];
-        }
+      let type = "audio";
+      if (!event.target.files[0].type.includes(type)) {
+        this.feedback = "Please upload an audio file and try again";
+      } else {
+        this.mp3Data = event.target.files[0];
+      }
     },
     onUpload() {
       function guid() {
@@ -99,25 +127,24 @@ export default {
           uuid: guid(),
         },
       };
-  if(!this.mp3Data){
-    this.feedback = 'Please select a file'
-  }
-  else{
-    feedback=''
-      const storageRef = storage
-        .ref(`mp3/${this.mp3Data.name}`)
-        .put(this.mp3Data, metadata);
-      storageRef.on(
-        `state_changed`,
-        (snapshot) => {
-          this.uploadValue =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        },
-        (error) => {
-          console.log(error.message);
-        }
-      );
-  }
+      if (!this.mp3Data) {
+        this.feedback = "Please select a file and try again";
+        } else {
+        this.feedback = "";
+        const storageRef = storage
+          .ref(`mp3/${this.yearPicked}/${this.mp3Data.name}`)
+          .put(this.mp3Data, metadata);
+        storageRef.on(
+          `state_changed`,
+          (snapshot) => {
+            this.uploadValue =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          },
+          (error) => {
+            console.log(error.message);
+          }
+        );
+      }
     },
   },
   computed: {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,11 +6,11 @@
 
   <!-- Dropdown Structure -->
   <ul id='dropdown1' class='dropdown-content'>
-    <li><a href="#!" v-on:click="speek(2020)">2020</a></li>
-    <li><a href="#!">2019</a></li>
+    <li><a href="#" v-on:click="Start('2020')">2020</a></li>
+    <li><a href="#" v-on:click="Start('2019')">2019</a></li>
    </ul>
         
-
+  <!-- Dropdown Structure -->
       <ul class="collapsible z-depth-0 list" style="border: none;">
         <li v-for="(file,index) in files" :key="index">
           <div class="row">
@@ -64,7 +64,7 @@ export default {
     return {
       files: [],
       string: "string",
-      upFiles: [],
+      upFiles: []
     };
   },
   computed: {
@@ -74,8 +74,19 @@ export default {
   },
   mounted() {
     M.AutoInit();
-
-    var folder = "mp3";
+  },
+  methods: {
+    send(file, name, event) {
+      event.target.disabled = true;
+      const transcribe = functions.httpsCallable("transcribe");
+      transcribe({ file: file, name: name }).then(() => {
+        alert("File submitted");
+      });
+    },
+    //new method here
+    Start(year){
+     
+    var folder = `/mp3/${year}`;
     var storageRef = storage.ref();
     var listRef = storageRef.child(folder);
     async function getFiles() {
@@ -105,24 +116,15 @@ export default {
         return await doc.data().text;
       }
     }
-    //ref should be sermon
+   
     async function getMetadata(ref) {
+       //ref should be sermon
       return ref.getMetadata();
     }
 
     getFiles().then((res) => (this.files = res));
-  },
-  methods: {
-    send(file, name, event) {
-      event.target.disabled = true;
-      const transcribe = functions.httpsCallable("transcribe");
-      transcribe({ file: file, name: name }).then(() => {
-        alert("File submitted");
-      });
-    },
-  },
-  speek: function (text){
-    alert(text)
+
+    }
   }
 };
 </script>
