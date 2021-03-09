7dac3360e40ba339c5592ea78d39f0425685ad67
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2021-03-08 20:03:23 -0500

    search working, no UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { FirebaseService } from '../services/firebase.service';
 import firebase from 'firebase/app';
 import { Direction } from './interfaces/HeroImage';
-import { AngularFirestore } from '@angular/fire/firestore';
-
 
 @Component({
   selector: 'app-root',
@@ -15,34 +13,14 @@ export class AppComponent {
   title = 'Life Church Lancaster Archive';
   tabs$: firebase.firestore.QueryDocumentSnapshot<any>[]
   img:Direction;
-  constructor(public firebaseService: FirebaseService, public db: AngularFirestore ) { }
-  searchArray=[];
+  constructor(public firebaseService: FirebaseService ) { }
+
   
   ngOnInit() {
       
     this.img = Direction.Home
       this.firebaseService.getYears().subscribe(e=>{
         this.tabs$ = e.docs;
-      })
-      
-    
-    
-      var sermonsThatHaveText = this.db.collectionGroup('items', ref=> ref.where('text', '!=', null))
-
-      sermonsThatHaveText.get().subscribe(docs=>{
-        docs.forEach(doc=>{
-          console.log(doc)
-          this.searchArray.push(doc.data())
-          console.log(this.searchArray)
-        })
-      })
-    
-      console.log(this.searchArray)
-    }
-        // console.log(e.docs[0].data()))
-          
-
-
-
-
+      })  
+  }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Direction } from '../interfaces/HeroImage';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
-
+  
+  constructor(public db: AngularFirestore) { }
+  img:Direction;
+  searchArray=[];
   ngOnInit(): void {
+    this.img = Direction.Search
+          
+
+    var sermonsThatHaveText = this.db.collectionGroup('items', ref=> ref.where('text', '!=', null))
+    sermonsThatHaveText.get()
+    .subscribe(docs=>{
+       docs.forEach(doc=>{
+          this.searchArray.push(doc.data())      
+      })
+  })
+
+
+  }
+
+
+  search(){
+    let newVar = this.searchArray.filter(it=>{
+      console.log(it)
+      return it.text.includes('hack');
+ }) 
+ console.log(newVar)
   }
 
 }
