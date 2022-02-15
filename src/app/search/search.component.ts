e47f544f324d2b4e541a695490f463c47c49dde4
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2022-02-15 15:14:40 -0500

    angular 13 fix

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollectionGroup } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollectionGroup } from '@angular/fire/compat/firestore';
 import { FirestoreRecord } from '../interfaces/FirestoreRecord';
 import { Direction } from '../interfaces/HeroImage';
 
@@ -9,7 +9,7 @@ import { Direction } from '../interfaces/HeroImage';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  
+
   constructor(public db: AngularFirestore) { }
   img:Direction;
   searchArray: Array<any> =[];
@@ -22,7 +22,7 @@ export class SearchComponent implements OnInit {
     sermonsThatHaveText.get()
     .subscribe(docs=>{
        docs.forEach(doc=>{
-          this.searchArray.push(doc.data())      
+          this.searchArray.push(doc.data())
       })
   })
   }
@@ -31,11 +31,11 @@ export class SearchComponent implements OnInit {
     this.search((target as HTMLInputElement).value.toLocaleLowerCase())
     return (target as HTMLInputElement).value;
   }
-  
+
   search(searchInput:string):void{
     this.filterSermons = this.searchArray.filter(item=>{
       return item.text.includes(searchInput);
-       }) 
+       })
   }
 
 }
